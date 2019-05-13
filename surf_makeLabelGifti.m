9bffc7323b0c598473343efb9d47f76b611d0e11
eberlot <eva.berlot@gmail.com>
2019-05-13 11:48:24 -0400

    Fixed surf_makeLabelGifti
    Minor changes: labelling of columns / label-names / colours

--- surf_makeLabelGifti.m
+++ surf_makeLabelGifti.m
@@ -1,32 +1,51 @@
 function G=surf_makeLabelGifti(data,varargin)
 % function G=surf_makeFuncGifti(data,varargin)
 % Builds a functional funtional GIFTI structure from scratch for saving 
+%
 % INPUT: 
 %   data: N x Q data matrix: N:number of nodes, Q: number of data series
+%
 % VARARGIN: 
 %   'anatomicalStruct': Anatomical Structure for header
 %                       'CortexLeft','CortexRight','Cerebellum' 
 %   'labelNames': Cell array of Names for the labels 
 %   'columnNames': Cell array of the Names of different data columns 
 %   'labelRGBA':  numLabel x 4 matrix of RGB + alpha (opacacity) values
+%
 % OUTPUT: 
 %   G: Gifti object - save with save(G,'filename')
-
+% ---------------------------------------------------------------------------------
 anatomicalStruct = 'CortexLeft'; 
 columnNames     = {}; 
 labelRGBA       = []; 
 labelNames      = {}; 
-
 vararginoptions(varargin,{'anatomicalStruct','columnNames','labelNames','labelRGBA'}); 
 
 [N,Q] = size(data);
-% Make column_names if empty 
+nLabel = length(unique(data)); % number of unique labels
+% Create naming and coloring if not given:
+% 1) Make column_names if empty 
 if (isempty(columnNames))
-    for i=1:Q 
+    for i=1:nLabel
         columnNames{i}=sprintf('col_%d',i);
     end; 
 end; 
-        
+% 2) Determine color scale if empty
+if (isempty(labelRGBA))
+    col = hsv(nLabel);
+    col = col(randperm(nLabel),:); % shuffle the order so it's more visible
+    labelRGBA = zeros(nLabel,4);
+    for i=1:nLabel
+        labelRGBA(i,:)=[col(i,:) 1];
+    end; 
+end
+% 3) Give label names
+if (isempty(labelNames))
+    for i=1:nLabel
+        labelNames{i}=sprintf('label-%d',i);
+    end; 
+end
+% Create the label.gii structure
 this.metadata(1) = struct('name','AnatomicalStructurePrimary','value',anatomicalStruct);  
 this.metadata(2) = struct('name','encoding','value','XML_BASE64_GZIP'); 
 this.label.name  = labelNames; 
