1f0657e899bcb0cc8e1f246dbec1c382fb2e29bb
saarbuckle <saarbuckle@gmail.com>
2019-05-14 16:36:31 -0400

    Create surf_resampleMetric.m
    Wrapper function to resample metric file into target atlas space

---
+++ surf_resampleMetric.m
@@ -0,0 +1,44 @@
+function surf_resampleMetric(hemisphere,inFile,outFile,atlasDir,varargin)
+% function G=surf_resampleMetric(data,varargin)
+% Builds a functional funtional GIFTI structure and resamples it to target
+% atlas space.
+% / / / / /
+% INPUT: 
+%   hemisphere :  left(1) or right(2). Currently no support to do both in one call
+%   inFile     :  Filename of metric file to be resampled
+%   outFile    :  Filename of metric file that has been resampled.
+%   atlasDir   :  Directory with files for the standard_mesh (Atlas_templates/standard_mesh)
+% / / / / /
+% VARARGIN: 
+%   'surf'     :  Number of vertices in surface which we use to resample
+%                  metric file. '32k', or '164k' (default = '32k')
+% SArbuckle 05/2019
+
+surf = '32k';
+vararginoptions(varargin,{'surf'});
+
+
+% error handling
+[~,~,ext] = fileparts(inFile);
+if ~strcmp(ext,'.metric')
+    error('inFile is not a .metric file.')
+end
+[~,~,ext] = fileparts(outFile);
+if~strcmp(ext,'.gii')
+    error('outFile is not a .gii file.')
+end
+
+% load .metric file and convert to gifti
+C = caret_load(inFile);
+G = surf_makeFuncGifti(C.data,'columnNames',C.column_name);
+save(G,outFile); % convert metric file to gifti and save temporarily (we overwrite this with resampled metric)
+
+% filenames for inflated surface spheres (we align inSphere to outSphere and then resample inFile)
+Hem       = {'L','R'}; 
+inSphere  = fullfile(atlasDir,['fs_' Hem{hemisphere}],['fsaverage.' Hem{hemisphere} '.sphere.164k_fs_' Hem{hemisphere} '.surf.gii']);
+outSphere = fullfile(atlasDir,'resample_fsaverage',['fs_LR-deformed_to-fsaverage.' Hem{hemisphere} '.sphere.' surf '_fs_LR.surf.gii']);
+
+% do resampling of metric file
+system(['wb_command -metric-resample ' outFile ' ' inSphere ' ' outSphere ' ADAP_BARY_AREA ' outFile ' -area-surfs ' inSphere ' ' outSphere]);
+
+end
